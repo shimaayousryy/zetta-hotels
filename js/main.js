9219c605664c1956ca0ca8fb1b0b2c68a9ab5991
shimaayousryy <shimaayousry29@gmail.com>
2021-06-22 20:20:05 +0200

    first add

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $('.demo').slick({
+        // infinite: false,
+        arrows:true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+          ]
+      });
+  });
+
